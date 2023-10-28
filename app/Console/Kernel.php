3718b85b403734ec9eb7f301102735c19e97ba6a
Hassaan Pasha <pasha.hassaan@gmail.com>
2023-10-28 12:19:21 +0500

    add blog screenshot scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('notion:read')->everyFifteenMinutes();
+        $schedule->command('blog:screenshot')->everyThirtyMinutes();
     }
 
     /**
